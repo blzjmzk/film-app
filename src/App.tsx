5bb74c2978a88a883cd5f42410ddb8732b8a96b6
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-12-18 12:01:43 +0100

    Instal DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <p className="text-red-500">Hello World</p>
+      <button class="btn">Button</button>
+      <button class="btn btn-neutral">Neutral</button>
+      <button class="btn btn-primary">Primary</button>
+      <button class="btn btn-secondary">Secondary</button>
+      <button class="btn btn-accent">Accent</button>
+      <button class="btn btn-ghost">Ghost</button>
+      <button class="btn btn-link">Link</button>
     </>
   );
 }
