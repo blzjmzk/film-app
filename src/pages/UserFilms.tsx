17d34ee4fb9e9509ccf3d6434447b00af005442d
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-12-29 17:32:57 +0100

    Added start message in My Movies

--- src/pages/UserFilms.tsx
+++ src/pages/UserFilms.tsx
@@ -7,6 +7,13 @@ interface Props {
 }
 
 const UserFilms = ({ watched, onDeleteWatched }: Props) => {
+  if (watched.length === 0) {
+    return (
+      <h2 className="text-center mt-16 mb-4 text-lg">
+        Rate a movie to add to My Movies
+      </h2>
+    );
+  }
   return (
     <>
       <div className="flex flex-row flex-wrap max-w-[900px] gap-5 p-10 mx-auto justify-center">
